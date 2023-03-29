48dab80e0489dfab4fdae76d953d1be4375a2418
Lucas La Tour <lucaslatour@Lucass-MBP-2.lan>
2023-03-28 20:10:48 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 var app = express();
 
@@ -11,7 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  let response = {"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size}
+  res.json(response);
+  next();
+})
 
 
 const port = process.env.PORT || 3000;
